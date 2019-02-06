a34b059b1fe1e5bd2edb04503013078cfcad4d8d
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-05 19:00:59 -0500

    The main class was empty before. Now it has a new method aliceAndBob, which greets users whose names are Alice or Bob.

--- Main.java
+++ Main.java
@@ -1,9 +1,33 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
-
+    
+    /**
+     * This main method simply calls the method aliceAndBob.
+     */
     public static void main(String[] args ){
-
+        aliceAndBob();
+    }
+    
+    /**
+     * This method asks the user what their name is. If the user's name is
+     * "Alice" or "Bob", then it greets the user with "Hello!". In every
+     * other case, it says "You are not Alice or Bob."
+     */
+    public static void aliceAndBob(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name?\n");
+        String name = input.next();
+        
+        if (name.equals("Alice") || name.equals("Bob")){
+            System.out.print("Hello!\n");
+        }
+        
+        else{
+            System.out.print("You are not Alice or Bob.\n");
+        }
     }
 }
